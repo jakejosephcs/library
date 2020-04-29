3d20ffdfeac756516f8930e5b55e2057906c7e9e
Jake Joseph <jakejosephcs@gmail.com>
2020-04-29 18:18:25 -0400

    Finish adding local storage

--- main.js
+++ main.js
@@ -1,29 +1,101 @@
 const form = document.querySelector('form');
-
 const addToLibraryBtn = document.querySelector('#addToLibrary');
 const hideFormBtn = document.querySelector('#hideForm');
 const addNewBookBtn = document.querySelector('#addNewBook')
 
-const container = document.querySelector('.container');
-
 let bookList = []
 
-function checkFormInput(author, title, pages, read) {
-    if (author == '' || title == '' || pages == '') {
-        return true;
+let BOOKS = (function() {
+    const changeReadStatus = function(index) {
+        bookList.forEach(book => {
+            if (book.id == index.slice(6)) toggleReadStatus(index)
+        })
+    };
+
+    const toggleReadStatus = function(index) {
+        let readStatusBtn = document.querySelector(`.${index}`);
+        if (readStatusBtn.classList.contains('btn-success')) {
+            readStatusBtn.classList.remove('btn-success');
+            readStatusBtn.classList.add('btn-danger');
+            readStatusBtn.textContent = 'NO';
+            bookList[index.slice(6)-1].read = false;
+        } else {
+            readStatusBtn.classList.remove('btn-danger');
+            readStatusBtn.classList.add('btn-success');
+            readStatusBtn.textContent = 'YES';
+            bookList[index.slice(6)-1].read = true;
+        }
+    };
+
+    const reAssignIds = function() {
+        bookList.forEach((book,i) => book.id = i + 1);
     }
-    return false;
-}
 
-function showAlert(message, className){
-    const alertMessage = document.createElement('div');
-    alertMessage.textContent = message
-    alertMessage.classList.add('alert', `alert-${className}`, 'bg-danger', 'mb-2', 'text-center')
-    container.insertBefore(alertMessage, form);
-    // Remove after 3s
-    setTimeout(() => document.querySelector('.alert').remove(), 2000)
-}
+    return {
+        changeReadStatus,
+        reAssignIds
+    }
+    
+})();
 
+let UI = (function() {
+    const checkFormInput = function(author, title, pages) {
+        if (author == '' || title == '' || pages == '') {
+            return true;
+        }
+        return false;
+    };
+
+    const showAlert = function(message, className) {
+        const alertMessage = document.createElement('div');
+        const container = document.querySelector('.container');
+        alertMessage.textContent = message
+        alertMessage.classList.add('alert', `alert-${className}`, 'bg-danger', 'mb-2', 'text-center')
+        container.insertBefore(alertMessage, form);
+        // Remove after 3s
+        setTimeout(() => document.querySelector('.alert').remove(), 2000)
+    };
+
+    const deleteBookEntry = function(index) {
+        bookList.forEach(book => {
+            if (book.id == index.slice(3)){
+                bookList.splice(index.slice(3)-1, 1)
+            }
+        })
+    };
+    
+    const refreshBookList = function() {
+        const tableBody = document.querySelector('tbody');
+        tableBody.innerHTML = "";
+        let readStatus;
+        let successOrDanger;
+        bookList.forEach(book => {
+            if (book.read) {
+                readStatus = "YES";
+                successOrDanger = "success";
+            } else {
+                readStatus = "NO";
+                successOrDanger = 'danger';
+            }
+            tableBody.innerHTML += `
+                <td>${book.author}</td>
+                <td>${book.title}</td>
+                <td>${book.pages}</td>
+                <td><button type="button" class="change${book.id} btn btn-${successOrDanger} btn-sm">${readStatus}</button></td>
+                <td><button type="button" class="del${book.id} btn btn-warning btn-sm">X</button></td>
+            `
+        })
+    };
+
+    return {
+        checkFormInput,
+        showAlert,
+        deleteBookEntry,
+        refreshBookList
+    }
+})();
+
+// CREATE: Creates new book
 class Books{
     constructor(author, title, pages, read){
         this.author = author;
@@ -33,27 +105,26 @@ class Books{
     }
 }
 
-function addBookToList(book) {
-    const tableBody = document.querySelector('tbody');
-    let readStatus;
-    let successOrDanger;
-    if (book.read) {
-        readStatus = "YES";
-        successOrDanger = "success";
-    } else {
-        readStatus = "NO";
-        successOrDanger = 'danger';
+let STORE = (function() {
+    // Sets the book list inside local storage
+    let setItem = function() {
+        localStorage.setItem("bookList", JSON.stringify(bookList));
+    };
+
+    // Retrives the book list from local storage
+    let getItem = function() {
+        bookList = JSON.parse(localStorage.getItem("bookList"));
+        bookList.forEach(book => {UI.refreshBookList()});
+    };
+
+    return {
+        setItem,
+        getItem
     }
-    tableBody.innerHTML += `
-        <td>${book.author}</td>
-        <td>${book.title}</td>
-        <td>${book.pages}</td>
-        <td><button type="button" class="change${book.id} btn btn-${successOrDanger} btn-sm">${readStatus}</button></td>
-        <td><button type="button" class="del${book.id} btn btn-warning btn-sm">X</button></td>
-    `
-}
 
+})();
 
+// EVENT: Add to Library
 addToLibraryBtn.addEventListener('click', () => {
     // Get book information
     const author = document.querySelector('#author').value;
@@ -61,83 +132,38 @@ addToLibraryBtn.addEventListener('click', () => {
     const pages = document.querySelector('#numOfPages').value;
     const read = document.querySelector('#read').checked;
 
-    if (checkFormInput(author, title, pages, read)) {
-        showAlert("Please Fill out the Form Properly", "danger")
+    // Checks if the form is empty, alerts if it is
+    if (UI.checkFormInput(author, title, pages)) {
+        UI.showAlert("Please Fill out the Form Properly", "danger")
         return;
     }
 
+    // Creates new book object based on inputs
     const book = new Books(author, title, pages, read)
 
+    // Add new book to book list
     bookList.push(book);
 
-    bookList.forEach((book,i) => book.id = i + 1);
+    // Iterate through the booklist to add a book id to each book
+    BOOKS.reAssignIds();
 
-    addBookToList(book);
+    // Adds book to the book list
+    UI.refreshBookList();
 
+    // Resets the form so old input isn't left inserted
     form.reset();
+
+    // Adds bookList to local storage
+    STORE.setItem();
 
 })
 
-function changeReadStatus(index) {
-    bookList.forEach(book => {
-        if (book.id == index.slice(6)) toggleReadStatus(index)
-    })
-}
-
-function toggleReadStatus(index){
-    let readStatusBtn = document.querySelector(`.${index}`);
-    if (readStatusBtn.classList.contains('btn-success')) {
-        readStatusBtn.classList.remove('btn-success');
-        readStatusBtn.classList.add('btn-danger');
-        readStatusBtn.textContent = 'NO';
-        bookList[index.slice(6)-1].read = false;
-    } else {
-        readStatusBtn.classList.remove('btn-danger');
-        readStatusBtn.classList.add('btn-success');
-        readStatusBtn.textContent = 'YES';
-        bookList[index.slice(6)-1].read = true;
-    }
-    
-}
-
-function deleteBookEntry(index){
-    bookList.forEach(book => {
-        if (book.id == index.slice(3)){
-            bookList.splice(index.slice(3)-1, 1)
-        }
-    })
-    
-}
-
-function refreshBookList(){
-    const tableBody = document.querySelector('tbody');
-    tableBody.innerHTML = "";
-    let readStatus;
-    let successOrDanger;
-    bookList.forEach(book => {
-        if (book.read) {
-            readStatus = "YES";
-            successOrDanger = "success";
-        } else {
-            readStatus = "NO";
-            successOrDanger = 'danger';
-        }
-        tableBody.innerHTML += `
-            <td>${book.author}</td>
-            <td>${book.title}</td>
-            <td>${book.pages}</td>
-            <td><button type="button" class="change${book.id} btn btn-${successOrDanger} btn-sm">${readStatus}</button></td>
-            <td><button type="button" class="del${book.id} btn btn-warning btn-sm">X</button></td>
-        `
-    })
-}
-
-// EVENT: Hide add new book form
+// EVENT: Hide "add new book" form
 hideFormBtn.addEventListener('click', () => {
     form.classList.toggle('hide');
 })
 
-// EVENT: Show add new book form
+// EVENT: Show "add new book" form
 addNewBookBtn.addEventListener('click', () => {
     form.classList.toggle('hide');
 })
@@ -146,9 +172,23 @@ addNewBookBtn.addEventListener('click', () => {
 // EVENT: Delete or Change Status of book from reading list
 document.querySelector('.book-list').addEventListener('click', (e) => {
     if (e.target.classList[2] == 'btn-warning') {
-        deleteBookEntry(e.target.classList[0]);
-        refreshBookList()
-    }else {
-        changeReadStatus(e.target.classList[0])
+        UI.deleteBookEntry(e.target.classList[0]);
+        // Reassigns ID's of reminaing book entries
+        BOOKS.reAssignIds()
+        // Updates displayed Book List
+        UI.refreshBookList();
+        // Updates local storage after book has been deleted
+        STORE.setItem();
+    }else if (e.target.classList[2] == "btn-success" || e.target.classList[2] == "btn-danger") {
+        BOOKS.changeReadStatus(e.target.classList[0]);
+        // Updates displayed Book List
+        UI.refreshBookList();
+        // Updates local storage after read status has change
+        STORE.setItem();
     }
 })
+
+// Grabs the local storage values if there are any
+if (localStorage.length > 0) {
+    STORE.getItem();
+} 
